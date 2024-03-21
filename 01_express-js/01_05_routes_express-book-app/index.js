5fec62253ff5aeda008d651e0fd2223616b581f2
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-03-21 22:28:33 +0100

    Add '/books' route to index.js

--- 01_express-js/01_05_routes_express-book-app/index.js
+++ 01_express-js/01_05_routes_express-book-app/index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import getBooks from "./services/books/getBooks.js"; //! use the file extension
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/books", (req, res) => {
+  // only simple error handling since we're not dealing with
+  // since we are not dealing with specific parameters (i.e. IDs etc.)
+  try {
+    const books = getBooks();
+    res.status(200).json(books);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send("Something went wrong while getting the list of books!");
+  }
 });
 
 app.listen(3000, () => {
