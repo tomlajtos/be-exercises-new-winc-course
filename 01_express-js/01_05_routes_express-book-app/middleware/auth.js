583ec1cbed9c8bbee1347dc3a57ff41f32632f0e
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-04-04 22:51:34 +0200

    Add authorization middleware
    - to be used to implement authentication & authorization on protected
      API routes, to make sure only users can: create, update and delete book entries

---
+++ 01_express-js/01_05_routes_express-book-app/middleware/auth.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+  // we use the same secretKey that we used for the JWK creation
+  const secretKey =
+    process.env.AUTH_SECRET_KEY ||
+    "my-obviously-not-very-secure-and-not-very-secret-key"; // we still need to provide a default value
+
+  // response for missing auth token in the auth header
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "You cannot access this operation without a token!" });
+  }
+
+  // use jwt.verify to check the token validity
+  // NOTE: we only saved the user ID in the token body, for more complex validation
+  // more data would be needed >> this would be best handled by a separate middleware
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token!" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+export default authMiddleware;
