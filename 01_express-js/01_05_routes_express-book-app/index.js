983806461ed96cdd05eada399fab8d6ead292bdb
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-04-04 02:51:50 +0200

    Add and use log midlleware globally in express book app

--- 01_express-js/01_05_routes_express-book-app/index.js
+++ 01_express-js/01_05_routes_express-book-app/index.js
@@ -7,15 +7,17 @@
 import express from "express";
 import booksRouter from "./routes/books.js";
 import recordsRouter from "./routes/records.js";
+import log from "./middleware/logMiddleware.js";
 
 const app = express();
 // needed for the express app to parse incomming JSON in the request body
 // (i.e. in post requests), it's a built in middleware function
 app.use(express.json());
+app.use(log);
 app.use("/books", booksRouter); // attach all routes from books.js router module >> this enables relative URL paths inside books router
 app.use("/records", recordsRouter); // attach all routes from records.js router module >> this enables relative URL paths inside books router
 
-app.get("/", (req, res) => {
+app.get("/", log, (req, res) => {
   const html = `<html><h1>Welcome to the Express Bookstore</h1> 
      <p>This is a test app to learn back-end programming with Express.js</p></html>`;
   res.send(html);
