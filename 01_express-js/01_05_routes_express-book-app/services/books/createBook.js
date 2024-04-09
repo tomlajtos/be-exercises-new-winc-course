558f7f28b174123008569d230539848f217795c4
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-04-09 22:09:17 +0200

    Add test 'throw Error..'

--- 01_express-js/01_05_routes_express-book-app/services/books/createBook.js
+++ 01_express-js/01_05_routes_express-book-app/services/books/createBook.js
@@ -3,6 +3,9 @@ import { v4 as uuid } from "uuid";
 import bookData from "../../data/books.json" assert { type: "json" };
 
 const createBook = (title, author, isbn, pages, available, genre) => {
+  // test throw -- for error handling
+  // throw new Error("Not implemented!");
+
   const newBook = {
     id: uuid(),
     title,
--- 01_express-js/01_05_routes_express-book-app/services/records/createRecord.js
+++ 01_express-js/01_05_routes_express-book-app/services/records/createRecord.js
@@ -1,6 +1,8 @@
 // use uuid package installed as dependency to generate unique record IDs;
 import { v4 as uuid } from "uuid";
 import recordData from "../../data/records.json" assert { type: "json" };
+// test throw -- for error handling
+// throw new Error("Not implemented!");
 
 const createRecord = (title, artist, year, available, genre) => {
   const newRecord = {
