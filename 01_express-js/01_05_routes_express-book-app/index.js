d88c4260fc3bf5c16c78ca6bdf138b04d499c93d
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-04-09 22:12:18 +0200

    Add code for Sentry

--- 01_express-js/01_05_routes_express-book-app/index.js
+++ 01_express-js/01_05_routes_express-book-app/index.js
@@ -6,13 +6,41 @@
 
 import express from "express";
 import "dotenv/config"; // needed for accessing environment variables
+import * as Sentry from "@sentry/node";
+// import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
 import booksRouter from "./routes/books.js";
 import recordsRouter from "./routes/records.js";
 import loginRouter from "./routes/login.js";
+
 import log from "./middleware/logMiddleware.js";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
+// init sentry at the earliest point of the app's lifecycle
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    // nodeProfilingIntegration(),
+    // Automatically instrument Node.js libraries and frameworks
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  // profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+// RequestHandler creates a separate execution context, so that all
+// transactions/spans/breadcrumbs are isolated across requests
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // needed for the express app to parse incomming JSON in the request body
 // (i.e. in post requests), it's a built in middleware function
@@ -30,7 +58,9 @@ app.get("/", log, (req, res) => {
   res.send(html);
 });
 
-// attach errorHandler after all other middleware and routs
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+// attach errorHandler after all other middleware and routes
 // should be the last element of the chain
 app.use(errorHandler);
 
