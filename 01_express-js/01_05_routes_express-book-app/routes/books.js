7e289b8fc211cd90f137c71ea3fb5286f1eca6d4
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-04-09 22:13:25 +0200

    refactor: use improved error handling via  notFoundErrorHandler middleware

--- 01_express-js/01_05_routes_express-book-app/routes/books.js
+++ 01_express-js/01_05_routes_express-book-app/routes/books.js
@@ -7,53 +7,39 @@ import getBookById from "../services/books/getBookById.js";
 import createBook from "../services/books/createBook.js";
 import updateBookById from "../services/books/updateBookById.js";
 import deleteBook from "../services/books/deleteBook.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   // only simple error handling since we're not dealing with
   // since we are not dealing with specific parameters (i.e. IDs etc.)
-  try {
-    const { genre, available } = req.query;
-    const books = getBooks(genre, available);
-    res.status(200).json(books);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send("Something went wrong while getting the list of books!");
-  }
-});
-
-router.get("/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = getBookById(id);
-
-    if (!book) {
-      res.status(404).send(`Book with id ${id} was not found!`);
-    } else {
-      res.status(200).json(book);
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong while getting the book by id!");
-  }
+  const { genre, available } = req.query;
+  const books = getBooks(genre, available);
+  res.status(200).json(books);
 });
 
 router.post("/", authMiddleware, (req, res) => {
-  try {
-    const { title, author, isbn, pages, available, genre } = req.body;
-    const newBook = createBook(title, author, isbn, pages, available, genre);
-    res.status(201).json(newBook);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong while creating the new book!");
-  }
+  const { title, author, isbn, pages, available, genre } = req.body;
+  const newBook = createBook(title, author, isbn, pages, available, genre);
+  res.status(201).json(newBook);
 });
 
-router.put("/:id", authMiddleware, (req, res) => {
-  try {
+router.get(
+  "/:id",
+  (req, res) => {
+    const { id } = req.params;
+    const book = getBookById(id);
+
+    res.status(200).json(book);
+  },
+  notFoundErrorHandler,
+);
+
+router.put(
+  "/:id",
+  authMiddleware,
+  (req, res) => {
     const { id } = req.params;
     const { title, author, isbn, pages, available, genre } = req.body;
     const updatedBook = updateBookById(
@@ -66,29 +52,23 @@ router.put("/:id", authMiddleware, (req, res) => {
       genre,
     );
     res.status(200).json(updatedBook);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong while updating the book by id!");
-  }
-});
+  },
+  notFoundErrorHandler,
+);
 
-router.delete("/:id", authMiddleware, (req, res) => {
-  try {
+router.delete(
+  "/:id",
+  authMiddleware,
+  (req, res) => {
+    // try {
     const { id } = req.params;
     const deletedBookId = deleteBook(id);
 
-    if (!deletedBookId) {
-      res.status(404).send(`Book with id ${id} was not found!`);
-    } else {
-      res.status(200).json({
-        message: `The book with id ${id} was deleted!`,
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    console.log("id of book to be deleted:", id);
-    res.status(500).send("Something went wrong while deleting book by id!");
-  }
-});
+    res.status(200).json({
+      message: `The book with id ${deletedBookId} was deleted!`,
+    });
+  },
+  notFoundErrorHandler,
+);
 
 export default router;
--- 01_express-js/01_05_routes_express-book-app/routes/records.js
+++ 01_express-js/01_05_routes_express-book-app/routes/records.js
@@ -7,55 +7,37 @@ import getRecordById from "../services/records/getRecordById.js";
 import createRecord from "../services/records/createRecord.js";
 import updateRecordById from "../services/records/updateRecordById.js";
 import deleteRecord from "../services/records/deleteRecord.js";
+import notFoundErrorHandler from "../middleware/notFoundErrorHandler.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  // only simple error handling since we're not dealing with
-  // since we are not dealing with specific parameters (i.e. IDs etc.)
-  try {
-    const { artist, genre, available } = req.query;
-    const records = getRecords(artist, genre, available);
-    res.status(200).json(records);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send("Something went wrong while getting the list of records!");
-  }
-});
-
-router.get("/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-    const record = getRecordById(id);
-
-    if (!record) {
-      res.status(404).send(`Record with id ${id} was not found!`);
-    } else {
-      res.status(200).json(record);
-    }
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send("Something went wrong while getting the record by id!");
-  }
+  const { artist, genre, available } = req.query;
+  const records = getRecords(artist, genre, available);
+  res.status(200).json(records);
 });
 
 router.post("/", authMiddleware, (req, res) => {
-  try {
-    const { title, artist, year, available, genre } = req.body;
-    const newRecord = createRecord(title, artist, year, available, genre);
-    res.status(201).json(newRecord);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Something went wrong while creating the new record!");
-  }
+  const { title, artist, year, available, genre } = req.body;
+  const newRecord = createRecord(title, artist, year, available, genre);
+  res.status(201).json(newRecord);
 });
 
-router.put("/:id", authMiddleware, (req, res) => {
-  try {
+router.get(
+  "/:id",
+  (req, res) => {
+    const { id } = req.params;
+    const record = getRecordById(id);
+
+    res.status(200).json(record);
+  },
+  notFoundErrorHandler,
+);
+
+router.put(
+  "/:id",
+  authMiddleware,
+  (req, res) => {
     const { id } = req.params;
     const { title, artist, year, available, genre } = req.body;
     const updatedRecord = updateRecordById(
@@ -67,31 +49,22 @@ router.put("/:id", authMiddleware, (req, res) => {
       genre,
     );
     res.status(200).json(updatedRecord);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send("Something went wrong while updating the record by id!");
-  }
-});
+  },
+  notFoundErrorHandler,
+);
 
-router.delete("/:id", authMiddleware, (req, res) => {
-  try {
+router.delete(
+  "/:id",
+  authMiddleware,
+  (req, res) => {
     const { id } = req.params;
     const deletedRecordId = deleteRecord(id);
 
-    if (!deletedRecordId) {
-      res.status(404).send(`Record with id ${id} was not found!`);
-    } else {
-      res.status(200).json({
-        message: `The record with id ${id} was deleted!`,
-      });
-    }
-  } catch (error) {
-    console.error(error);
-    console.log("id of record to be deleted:", id);
-    res.status(500).send("Something went wrong while deleting record by id!");
-  }
-});
+    res.status(200).json({
+      message: `The record with id ${deletedRecordId} was deleted!`,
+    });
+  },
+  notFoundErrorHandler,
+);
 
 export default router;
