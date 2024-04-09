c9b2094083d3c0e039eb3e08ec941fff90ee0730
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-04-09 22:07:33 +0200

    Remove logger error

--- 01_express-js/01_05_routes_express-book-app/middleware/logMiddleware.js
+++ 01_express-js/01_05_routes_express-book-app/middleware/logMiddleware.js
@@ -10,9 +10,5 @@ const log = (req, res, next) => {
   logger.info(
     `method: ${req.method}, URL: ${req.originalUrl}, status: ${res.statusCode}, time to process (ms): ${processTimeInMs.toFixed(3)}`,
   );
-
-  if (res.statusCode >= 400) {
-    logger.error(`${res.statusCode}: ${res.statusMessage}`);
-  }
 };
 export default log;
