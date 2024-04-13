f2e886cc23d6a8cf7ddd58cca20c539304f1f4ab
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-04-13 22:37:02 +0200

    Add routers (events, categ.s, users) to express app

--- 01_express-js/01_06_express-events-app/src/index.js
+++ 01_express-js/01_06_express-events-app/src/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import usersRouter from "./routes/users.js";
+import eventsRouter from "./routes/events.js";
+import categoriesRouter from "./routes/categories.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/users", usersRouter);
+app.use("/events", eventsRouter);
+app.use("/categories", categoriesRouter);
 
 app.get("/", (req, res) => {
   res.send("Express Events REST API");
