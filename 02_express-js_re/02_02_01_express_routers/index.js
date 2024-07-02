9b2a1bb4ce812301cf28f9838527858b39893c22
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-07-02 14:57:27 +0200

    Add basic route for '/books' to index.js

--- 02_express-js_re/02_02_01_express_routers/index.js
+++ 02_express-js_re/02_02_01_express_routers/index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import getBooks from "./services/books/getBooks.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello again, World");
+});
+
+app.get("/books", (req, res) => {
+  try {
+    const books = getBooks();
+    res.status(200).json(books);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send("Something went wrong while getting the list of books!");
+  }
 });
 
 app.listen(3000, () => {
