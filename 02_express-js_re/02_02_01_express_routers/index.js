5e9226ee1c67f347c1c80a20316febd8189d6167
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2024-07-03 15:28:44 +0200

    Add basic rout for '/books/:id' to index.js
    Add new route to get a book with a specific id

--- 02_express-js_re/02_02_01_express_routers/index.js
+++ 02_express-js_re/02_02_01_express_routers/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import getBooks from "./services/books/getBooks.js";
+import getBookById from "./services/books/getBookById.js";
 
 const app = express();
 
@@ -16,6 +17,26 @@ app.get("/books", (req, res) => {
     res
       .status(500)
       .send("Something went wrong while getting the list of books!");
+  }
+});
+
+app.get("/books/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = getBookById(id);
+
+    if (!book) {
+      res.status(404).send(`The book with id ${id} was not found!`);
+    } else {
+      res.status(200).json(book);
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        "Something went wrong while getting the book with the specified id!",
+      );
   }
 });
 
